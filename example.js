4372525818d2371ad7840c126e496813d00709c7
dingo sky <paul@dingosky.com>
2017-07-25 17:33:01 -0700

    Add to example

--- example.js
+++ example.js
@@ -1,26 +1,31 @@
 const entropy = require('./entropy-string')
+const print = console.log
 
 let bits = 48
-console.log('\n48-bit string using base32 characters')
-console.log('  ' + entropy.string(bits, entropy.charSet32))
+print('\n48-bit string using base32 characters')
+print('  ' + entropy.string(bits, entropy.charSet32))
 
-console.log('\n48-bit string using hex characters')
-console.log('  ' + entropy.string(bits, entropy.charSet16))
+print('\n48-bit string using hex characters')
+print('  ' + entropy.string(bits, entropy.charSet16))
 
-console.log('\n48-bit string using uppercase hex characters')
+print('\n48-bit string using uppercase hex characters')
 entropy.charSet16.use('1234567890ABCDEF')
-console.log('  ' + entropy.string(bits, entropy.charSet16))
+print('  ' + entropy.string(bits, entropy.charSet16))
 
-console.log('\nBase 32 character string with a 1 in a million chance of a repeat in 30 such strings')
+print('\nBase 32 character string with a 1 in a million chance of a repeat in 30 such strings')
 bits = entropy.bits(30, 1000000)
-console.log('  ' + entropy.string(bits, entropy.charSet32))
+print('  ' + entropy.string(bits, entropy.charSet32))
 
-console.log('\nBase 32 character string with a 1 in a trillion chance of a repeat in 10 million such strings')
+print('\nBase 32 character string with a 1 in a trillion chance of a repeat in 10 million such strings')
 bits = entropy.bitsWithPowers(7, 12)
-console.log('  ' + entropy.string(bits, entropy.charSet32))
+print('  ' + entropy.string(bits, entropy.charSet32))
+
+print('\nAs above, but with "my" characters"')
+entropy.charSet8.use('dingosky')
+print('  ' + entropy.string(bits, entropy.charSet8))
 
 bits = 128
-console.log('\nOWASP session ID using file system and URL safe characters')
-console.log('  ' + entropy.string(bits, entropy.charSet64))
+print('\nOWASP session ID using file system and URL safe characters')
+print('  ' + entropy.string(bits, entropy.charSet64))
 
-console.log('')
+print('')
